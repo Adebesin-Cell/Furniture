f27bbf0acebbd957da67029879f69fb8c1ec37f2
adebesin-cell <adebesintolulope80@gmail.com>
2021-04-23 14:22:12 +0100

    added functionality to mobile nav

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const harmburger = document.querySelector(".harmburger");
 const closeButton = document.querySelector(".close");
 const headerBox = document.querySelector(".mobile-header__box");
 const linkItem = document.querySelectorAll(".mobile-header__navigation li");
+const navigationLink = document.querySelectorAll(".mobile-header__navigation__link");
+const buttonBox = document.querySelector(".mobile-header__btn-box");
+
 // console.log(harmburger, closeButton, headerBox, linkItem);
 harmburger.addEventListener("click", () => {
       headerBox.classList.add("open");
@@ -14,5 +17,19 @@ closeButton.addEventListener("click", () => {
       headerBox.classList.remove("open");
       linkItem.forEach(link => {
             link.classList.remove("fade");
+      })
+})
+
+// navigationLink.addEventListener ("click", () => {
+//       navigationLink.forEach(navLink => {
+//             navLink.classList.remove("open");
+//       })
+// })
+buttonBox.addEventListener ("click", () => {
+      headerBox.classList.remove("open");
+})
+navigationLink.forEach (navlink => {
+      navlink.addEventListener("click", () => {
+            headerBox.classList.remove("open");
       })
 })
