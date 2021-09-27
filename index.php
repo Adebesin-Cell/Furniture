352e3158f630647c2e47c885c6e481075f811e82
Adebesin-Cell <adebesintolulope80@gmail.com>
2021-09-27 19:58:00 +0100

    updating UI

--- index.php
+++ index.php
@@ -77,6 +77,14 @@
                         </li>
                     </ul>
                 </div>
+                <div class="header__icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <g>
+                            <path fill="none" d="M0 0h24v24H0z"/>
+                            <path d="M3 4h18v2H3V4zm6 7h12v2H9v-2zm-6 7h18v2H3v-2z"/>
+                        </g>
+                    </svg>
+                </div>
             </div>
         </div>
         <div class="header__overlay">
