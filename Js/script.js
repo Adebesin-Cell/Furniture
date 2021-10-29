81c070b89a219e98aec28cbdeab49c82659dc4da
Adebesin-Cell <adebesintolulope80@gmail.com>
2021-10-29 23:20:49 +0100

    updating UI

--- Js/script.js
+++ Js/script.js
@@ -24,3 +24,25 @@
 //     localStorage.setItem("theme", "light");
 //   }
 // });
+
+const tabContainer = document.querySelector(".overview");
+const tabsButton = document.querySelectorAll(".overview__btn");
+const tabsContent = document.querySelectorAll(".overview__wrapper");
+
+tabContainer.addEventListener("click", function (e) {
+  let id = e.target.dataset.category;
+  if (!id) return;
+
+  tabsButton.forEach((tabBtn) => {
+    tabBtn.classList.remove("overview__btn--active");
+  });
+
+  e.target.classList.add("overview__btn--active");
+
+  tabsContent.forEach((tabContent) => {
+    tabContent.classList.remove("overview__wrapper--active");
+  });
+
+  const contentElement = document.querySelector(`#${id}`);
+  contentElement.classList.add("overview__wrapper--active");
+});
